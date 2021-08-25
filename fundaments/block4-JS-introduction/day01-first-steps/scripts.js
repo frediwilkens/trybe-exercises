51a02f10774a846ef67ae20c453bf934606edbf6
Fredi Wilkens <frediwilkens@gmail.com>
2021-08-25 15:43:48 -0300

    part 2 primitive types and aritimetic operations

--- fundaments/block4-JS-introduction/day01-first-steps/scripts.js
+++ fundaments/block4-JS-introduction/day01-first-steps/scripts.js
@@ -8,21 +8,37 @@
 */
 
 /*1*/
-const myName = "Frederico";
+//const myName = "Frederico";
 /*2*/
-const birthCity = "Porto Alegre";
+//const birthCity = "Porto Alegre";
 /*3*/
-let birthYear = 1994;
+//let birthYear = 1994;
 
 /*4*/
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+//console.log(myName);
+//console.log(birthCity);
+//console.log(birthYear);
 
 /*5*/
-birthYear = 2030;
-console.log(birthYear);
+//birthYear = 2030;
+//console.log(birthYear);
 
 /*6*/
-birthCity = "Londres";
+//birthCity = "Londres";
 /*Error explanation: birthCity is a constant not a variable so is not possible to attribute a new value. */
+
+/* PARTE 2 
+1.Crie uma costante chamada base e uma variável chamada height e .atribua os seus respectivos valores: 5 e 8.
+2.Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+3.Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+*/
+
+/*1*/
+const base = 5;
+let height = 8;
+/*2*/
+const area = base * height;
+console.log(area);
+/*3*/
+const perimeter = (base * 2) + (height * 2);
+console.log(perimeter);
