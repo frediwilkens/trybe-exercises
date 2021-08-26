7a4b6255ef74ffb9c130b3ef4a73f08f1df2302a
Fredi Wilkens <frediwilkens@gmail.com>
2021-08-26 19:09:46 -0300

    Bonus exercise 1 solved.

--- fundaments/block4-JS-introduction/day02-array-loop-for/day02-exercises.js
+++ fundaments/block4-JS-introduction/day02-array-loop-for/day02-exercises.js
@@ -82,3 +82,37 @@ console.log(twentyFive);
 for (index = 0; index < twentyFive.length; index += 1){
     console.log(twentyFive[index] / 2);
 }
+
+/*
+Bonus
+
+Para os próximos dois exercícios leia este artigo e tente entender o que está acontencedo no código abaixo:
+
+for (let index = 1; index < array.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (array[index] < array[secondIndex]) {
+      let position = array[index];
+      array[index] = array[secondIndex];
+      array[secondIndex] = position;
+    }
+  }
+}
+
+1. Ordene o array numbers em ordem crescente e imprima seus valores;
+2. Ordene o array numbers em ordem decrescente e imprima seus valores;
+3. Agora crie um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push . O resultado deve ser o array abaixo:
+
+[45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+*/
+
+//1
+for (index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers[index] < numbers[secondIndex]) {
+        let position = numbers[index];
+        numbers[index] = numbers[secondIndex];
+        numbers[secondIndex] = position;
+      }
+    }
+}
+console.log(numbers);
