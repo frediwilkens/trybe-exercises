a0186ca0c6f3ad451f8b9b5fd898a47b36398a4f
Fredi Wilkens <frediwilkens@gmail.com>
2021-08-26 19:22:39 -0300

    Bonus exercise 2 solved

--- fundaments/block4-JS-introduction/day02-array-loop-for/day02-exercises.js
+++ fundaments/block4-JS-introduction/day02-array-loop-for/day02-exercises.js
@@ -116,3 +116,17 @@ for (index = 1; index < numbers.length; index += 1) {
     }
 }
 console.log(numbers);
+
+//2 OBS:criei um array similar para não interferir no exercício seguinte.
+
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (index = 1; index < numbers2.length; index += 1) {
+    for (secondIndex = 0; secondIndex < index; secondIndex += 1) {
+        if (numbers2[index] > numbers2[secondIndex]) {
+            let trocaonumero = numbers2[index];
+            numbers2[index] = numbers2[secondIndex];
+            numbers2[secondIndex] = trocaonumero;
+        }
+    }
+}
+console.log(numbers2);
