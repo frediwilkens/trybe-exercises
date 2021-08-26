a6322079b8075648169d52bf684eb666849549cf
Fredi Wilkens <frediwilkens@gmail.com>
2021-08-26 15:42:07 -0300

    Exercise 2 solved.

--- fundaments/block4-JS-introduction/day02-array-loop-for/day02-exercises.js
+++ fundaments/block4-JS-introduction/day02-array-loop-for/day02-exercises.js
@@ -25,3 +25,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let i = 0; i < numbers.length; i += 1){
     console.log(numbers[i])
 }
+
+//2
+let somaDosValores = 0;
+for(let indice = 0; indice < numbers.length; indice += 1){
+    somaDosValores = somaDosValores + numbers[indice];
+}
+console.log(somaDosValores);
